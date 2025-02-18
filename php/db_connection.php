ad7f7d8b7d1a071330a9bcee80ec1c3cf0a2a8a4
maxhermon <maxhermon1@gmail.com>
2025-02-18 11:18:23 +0000

    db changes

--- php/db_connection.php
+++ php/db_connection.php
@@ -3,7 +3,7 @@
 function connectToDatabase() {
 static $db = null;
     if ($db === null) {
-        $db = new SQLite3('../XLN_new_DB.db');
+        $db = new SQLite3('../data/XLN_new_DBA.db');
         if (!$db) {
             die("Database connection error: " . $db->lastErrorMsg());
         }
