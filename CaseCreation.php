7a84dfd1a983ee870b96d5f22928aabd4acddcc3
CCray21 <charlie.bcray@gmail.com>
2025-02-18 10:25:55 +0000

    CaseCreation.php now links to css properly

--- CaseCreation.php
+++ CaseCreation.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Case</title>
-    <link rel="stylesheet" href="CaseCreation.css">
+    <link rel="stylesheet" href="../css/CaseCreation.css">
 </head>
 <body>
 
