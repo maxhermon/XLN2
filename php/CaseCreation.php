ec2bc47961b6063f52c3040ac7d6d94a0ffe8134
maxhermon <maxhermon1@gmail.com>
2025-02-19 14:42:11 +0000

    Update CaseCreation.php

--- php/CaseCreation.php
+++ php/CaseCreation.php
@@ -123,7 +123,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Case Created</title>
-    <link rel="stylesheet" href="../css/CaseCreated.css">
+    <link rel="stylesheet" href="../css/CaseCreation.css">
     <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
