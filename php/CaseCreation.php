60bd372e776d9735074e4e92a63c9b46f7c49d64
maxhermon <maxhermon1@gmail.com>
2025-02-19 14:36:27 +0000

    Update CaseCreation.php

--- php/CaseCreation.php
+++ php/CaseCreation.php
@@ -122,22 +122,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Case</title>
-    <link rel="stylesheet" href="../css/CaseCreation.css">
+    <title>Case Created</title>
+    <link rel="stylesheet" href="../css/CaseCreated.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+  />
 </head>
 <body>
- 
     <header>
-        <img class="logo" src="../xlnLogo.png" alt="XLN Logo">
+        <a href="../html/Homepage.html"><img class="logo" src="../xlnLogo.png" alt="XLN Logo"></a>
         <nav>
-            <ul>
-                <li><a href="#">MyAccount</a></li>
-                <li><a href="#">XLN Home</a></li>
-                <li><a href="#">Contact</a></li>
+            <ul class="left-menu">
+                <li><a href="../html/Homepage.html"><i class="fa-solid fa-house"></i> XLN Home</a></li>
+                <li><a href="../html/Contact.html"><i class="fa-solid fa-envelope"></i> Contact</a></li>
+            </ul>
+            <ul class="right-menu">
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn"><i class="fa-solid fa-circle-user"></i> MyAccount</a>
+                    <div class="dropdown-content">
+                        <a href="ProfilePage.php">View Profile</a>
+                        <a href="logOut.php">Logout</a>
+                    </div>
+                </li>
             </ul>
         </nav>
     </header>
- 
     <main>
         <h2>Create a Case</h2>
         <form action="CaseCreation.php" method="POST">
