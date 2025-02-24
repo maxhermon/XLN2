49f19ad09a9272fc134e1b49de1fa30476c453f6
CCray21 <charlie.bcray@gmail.com>
2025-02-24 17:02:22 +0000

    css finished for change password

--- php/ResetPasswordPage.php
+++ php/ResetPasswordPage.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password</title>
     <link rel="stylesheet" href="../css/ResetPassword.css">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 </head>
 <body>
     <header>
         <a href="Homepage.php"><img class="logo" src="../xlnLogo.png" alt="XLN Logo"></a>
+        <a href="../html/Contact.html"><i class="fa-solid fa-envelope"></i> Contact</a>
     </header>
     <main>
-        <h1>Reset Password</h1>
         <form action="../php/ResetPasswordPage.php" method="POST">
+            <h1>Reset Password</h1>
             <label for="newPassword"><b>New Password</b></label>
             <input type="password" id="newPassword" name="newPassword" required>
             <label for="confirmPassword"><b>Confirm Password</b></label>
