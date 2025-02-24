ea944d8c24e2939b6cd21f25fef3a3f4b447dada
CCray21 <charlie.bcray@gmail.com>
2025-02-24 15:33:42 +0000

    chazza

--- php/InputEmailForPasswordReset.php
+++ php/InputEmailForPasswordReset.php
@@ -4,24 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password</title>
-    <link rel="stylesheet" href="../css/InputEmail.css">
+    <link rel="stylesheet" href="../css/LoginPage.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     <header>
         <a href="Homepage.php"><img class="logo" src="../xlnLogo.png" alt="XLN Logo"></a>
     </header>
     <main>
-        <form action="ResetPassword.php" method="POST">
+        <form id="resetPasswordForm">
             <label for="email"><b>Email address</b></label>
             <input type="email" id="email" name="email" required>
             <button type="submit">Send email</button>
         </form>
+        <div id="message" style="display:none;"></div>
     </main>
     <footer>
         <p>&copy; <span id="year"></span> XLN</p>
     </footer>
     <script>
         document.getElementById("year").innerHTML = new Date().getFullYear();
+
+        $(document).ready(function() {
+            $('#resetPasswordForm').on('submit', function(e) {
+                e.preventDefault();
+                var email = $('#email').val();
+                $.ajax({
+                    type: 'POST',
+                    url: 'ResetPassword.php',
+                    data: { email: email },
+                    success: function(response) {
+                        $('#message').html(response).show();
+                    },
+                    error: function() {
+                        $('#message').html('An error occurred. Please try again.').show();
+                    }
+                });
+            });
+        });
     </script>
 </body>
 </html>
