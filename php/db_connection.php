83f60e851b75ae6b343bcd8d3cb2ffa1d0ca356b
maxhermon <maxhermon1@gmail.com>
2025-03-27 14:40:29 +0000

    f

--- php/db_connection.php
+++ php/db_connection.php
@@ -11,10 +11,11 @@ function connectToDatabase() {
     return $db;
 }
 
-function logActivity($userID, $activity, $status) {
+function logActivity($userID, $caseID, $activity, $status) {
     $db = connectToDatabase();
-    $stmt = $db->prepare("INSERT INTO activities (userID, activity, date, status) VALUES (:userID, :activity, :date, :status)");
+    $stmt = $db->prepare("INSERT INTO activities (userID, caseID, activity, date, status) VALUES (:userID, :caseID, :activity, :date, :status)");
     $stmt->bindValue(':userID', $userID, SQLITE3_INTEGER);
+    $stmt->bindValue(':userID', $caseID, SQLITE3_INTEGER);
     $stmt->bindValue(':activity', $activity, SQLITE3_TEXT);
     $stmt->bindValue(':date', date('Y-m-d H:i:s'), SQLITE3_TEXT);
     $stmt->bindValue(':status', $status, SQLITE3_TEXT);
